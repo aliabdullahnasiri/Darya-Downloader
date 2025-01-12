550b6ed4a7c4b502333dc5794b88488bbbabb9f7
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-01-12 21:16:47 +0430

    main.py file modified.

---
+++ main.py
@@ -0,0 +1,47 @@
+import pathlib
+from typing import Literal, Union
+
+import click
+
+from darya import Darya
+
+
+@click.group()
+def main() -> None: ...
+
+
+@main.command()
+@click.option(
+    "--item-id",
+    required=True,
+    help="The unique identifier of the media item to download. This ID is used to locate and retrieve the specific media file or representation.",
+)
+@click.option(
+    "--resolution",
+    type=click.Choice(
+        ["1920x1080", "1280x720", "854x480", "426x240"], case_sensitive=False
+    ),
+    default="1920x1080",
+    help="Specify the media representation resolution to download.",
+)
+@click.option(
+    "--output",
+    type=click.Path(),
+    help="Specify the output file path.",
+)
+def download(
+    item_id: str,
+    resolution: Literal[
+        "1920x1080",
+        "1280x720",
+        "854x480",
+        "426x240",
+    ] = "1920x1080",
+    output: Union[pathlib.Path, None] = None,
+) -> None:
+    darya: Darya = Darya(item_id)
+    darya.download(resolution, output)
+
+
+if __name__ == "__main__":
+    main()
