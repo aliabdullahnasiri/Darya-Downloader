f3b5e195d9a27fb3f76f462cb016f443a38dc4d6
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-01-13 17:07:30 +0430

    darya.py file modified.

--- darya.py
+++ darya.py
@@ -1,12 +1,15 @@
+import io
 import os
-import uuid
 import pathlib
+import uuid
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
-from typing import Dict, List, Literal, Self, Union
+from typing import Dict, List, Literal, Self, Tuple, Union
+from urllib.parse import urlparse
 
 import ffmpeg
 import requests
+from rich.prompt import Prompt
 
 from console import console
 from functions import audio_bitrate2representation as ab2r
@@ -17,20 +20,31 @@ from logger import logger
 
 @dataclass
 class Darya:
-    item_id: str
+    item_identity: str
 
     def __post_init__(self: Self) -> None:
-        if not os.path.exists("downloads"):
-            os.mkdir("downloads")
+        self.DOWNLOAD_DIR: str = "downloads"
+        self.ITEM_DIRECTORY: str = f"{self.DOWNLOAD_DIR}/{self.item_identity}"
+        self.ITEM_OUTPUT_DIR: str = f"{self.ITEM_DIRECTORY}/output"
+        self.MPDS_OUTPUT_DIR: str = f"{self.ITEM_DIRECTORY}/mpds"
+        self.VIDEO_OUTPUT_DIR: str = f"{self.ITEM_DIRECTORY}/video"
+        self.AUDIO_OUTPUT_DIR: str = f"{self.ITEM_DIRECTORY}/audio"
+        self.THUMBNAIL_OUTPUT_DIR: str = f"{self.ITEM_DIRECTORY}/thumbnail"
+        self.BG_OUTPUT_DIR: str = f"{self.ITEM_DIRECTORY}/background"
 
-        paths = ["audio", "video", "mpds", "output"]
-        for path in paths:
-            if not os.path.exists(f"downloads/{path}"):
-                os.mkdir(f"downloads/{path}")
+        os.makedirs(self.DOWNLOAD_DIR, exist_ok=True)
+        os.makedirs(self.ITEM_DIRECTORY, exist_ok=True)
+        os.makedirs(self.ITEM_OUTPUT_DIR, exist_ok=True)
+        os.makedirs(self.ITEM_OUTPUT_DIR, exist_ok=True)
+        os.makedirs(self.MPDS_OUTPUT_DIR, exist_ok=True)
+        os.makedirs(self.VIDEO_OUTPUT_DIR, exist_ok=True)
+        os.makedirs(self.AUDIO_OUTPUT_DIR, exist_ok=True)
+        os.makedirs(self.THUMBNAIL_OUTPUT_DIR, exist_ok=True)
+        os.makedirs(self.BG_OUTPUT_DIR, exist_ok=True)
 
     def get_item(self: Self) -> Union[Dict, None]:
         response: requests.Response = requests.get(
-            f"https://ffprod2s3.b-cdn.net/c/278/catalog/d_35ePcIfifmsHJgZ7G_Yw/item/{self.item_id}.json"
+            f"https://ffprod2s3.b-cdn.net/c/278/catalog/d_35ePcIfifmsHJgZ7G_Yw/item/{self.item_identity}.json"
         )
 
         if response.status_code == 200:
@@ -132,6 +146,141 @@ class Darya:
 
         return paths
 
+    def download_media(
+        self: Self,
+        mpd: pathlib.Path,
+        base: str,
+        resolution: Literal[
+            "1920x1080", "1280x720", "854x480", "426x240"
+        ] = "1920x1080",
+        audio: Literal["128k", "256k", "320k"] = "128k",
+    ) -> Union[Tuple[bytes, bytes], None]:
+        representations = self.get_representations(str(mpd), base)
+        vrepr_id = r2r(resolution)
+        arepr_id = ab2r(audio)
+        representations = list(
+            filter(
+                lambda representation: representation["mime-type"] == "video/mp4"
+                and representation["representation-id"] == vrepr_id
+                or representation["mime-type"] == "audio/mp4"
+                and representation["representation-id"] == arepr_id,
+                representations,
+            )
+        )
+
+        if len(representations) == 2:
+            video_links: List[str] = []
+            audio_links: List[str] = []
+
+            for representation in representations:
+                init_file = representation["init"]
+                mime_type = representation["mime-type"]
+                segments = representation["segments"]
+
+                if mime_type == "video/mp4":
+                    video_links.append(init_file)
+                    video_links.extend(segments)
+                elif mime_type == "audio/mp4":
+                    audio_links.append(init_file)
+                    audio_links.extend(segments)
+
+            video_bytes: bytes = b""
+            audio_bytes: bytes = b""
+
+            for video_link in video_links:
+                filename = os.path.basename(urlparse(video_link).path)
+                path = f"{self.VIDEO_OUTPUT_DIR}/{filename}"
+
+                if not os.path.exists(path):
+                    download_file(video_link, path)
+                else:
+                    logger.info(f"The file '{path}' already exists.")
+
+                if os.path.exists(path):
+                    with open(path, "rb") as f:
+                        video_bytes += f.read()
+
+            for audio_link in audio_links:
+                filename = os.path.basename(urlparse(audio_link).path)
+                path = f"{self.AUDIO_OUTPUT_DIR}/{filename}"
+
+                if not os.path.exists(path):
+                    download_file(audio_link, path)
+                else:
+                    logger.info(f"The file '{path}' already exists.")
+
+                if os.path.exists(path):
+                    with open(path, "rb") as f:
+                        audio_bytes += f.read()
+
+            return video_bytes, audio_bytes
+
+        else:
+            logger.warning(
+                "No matching video or audio representation ID was found. Please check the provided resolution or bitrate."
+            )
+
+    def merge_media(
+        self: Self, video: bytes, audio: bytes, output: pathlib.Path
+    ) -> Union[pathlib.Path, None]:
+        if output.exists():
+            choice = Prompt.ask(
+                "The output file '{output}' has been created. Do you want to keep it?",
+                choices=["yes", "no"],
+                show_default=True,
+                default="yes",
+            )
+
+            if choice == "no":
+                os.remove(output)
+
+                logger.info(
+                    "Output file '{output}' has been deleted at the user's request."
+                )
+
+            else:
+                logger.info(f"Output file '{output}' has been kept.")
+
+        if not output.exists():
+            try:
+                logger.info(f"Starting merge of video and audio into '{output}'.")
+
+                video_path: pathlib.Path = pathlib.Path(
+                    f"/tmp/{self.item_identity}-{uuid.uuid4()}.mp4"
+                )
+                audio_path: pathlib.Path = pathlib.Path(
+                    f"/tmp/{self.item_identity}-{uuid.uuid4()}.mp3"
+                )
+
+                with open(video_path, "wb") as f:
+                    f.write(video)
+
+                with open(audio_path, "wb") as f:
+                    f.write(audio)
+
+                # Using ffmpeg-python to merge audio and video
+                ffmpeg.output(
+                    ffmpeg.input(video_path),
+                    ffmpeg.input(audio_path),
+                    str(output),
+                    vcodec="copy",  # Copy the video stream without re-encoding
+                    acodec="aac",  # Encode the audio in AAC format
+                ).run()
+
+                if output.exists():
+                    logger.success(f"Successfully merged into '{output}'.")
+
+                    return output
+                else:
+                    logger.error(
+                        f"Merge failed: Output file '{output}' was not created."
+                    )
+
+            except ffmpeg.Error as e:
+                logger.error(f"An error occurred while merging: {e.stderr.decode()}")
+            except Exception as ex:
+                logger.exception(f"An unexpected error occurred: {ex}")
+
     def download(
         self: Self,
         resolution: Literal[
@@ -142,128 +291,25 @@ class Darya:
     ) -> None:
         item = self.get_item()
 
-        if item:
-            item_id = item.get("mediaID") or item.get("trailerID")
-            if item_media := item.get("media") or item.get("trailer"):
-                mpds: List[pathlib.Path] = self.download_mpds(item_media["mpds"])
+        if output is None:
+            output = pathlib.Path(f"{self.ITEM_OUTPUT_DIR}/{self.item_identity}.mp4")
 
-                en_title: str = item["title"]["en"]
-                mpd: Union[pathlib.Path, None] = choose_mpd_file("downloads/mpds")
+        if item and (item_media := item.get("media") or item.get("trailer")):
+            media_identity = item.get("mediaID") or item.get("trailerID")
+            mpds = item_media["mpds"]
+            mpd = download_file(
+                link := mpds.pop(0),
+                f"{self.MPDS_OUTPUT_DIR}/{os.path.basename(urlparse(link).path)}",
+            )
 
-                if mpd:
-                    representations = self.get_representations(
-                        str(mpd),
-                        f"https://ffprod2.b-cdn.net/c/278/m/{item_id}.ism/",
-                    )
-                    vrepr_id = r2r(resolution)
-                    arepr_id = ab2r(audio)
+            if media_identity and mpd:
+                media = self.download_media(
+                    mpd,
+                    f"https://ffprod2.b-cdn.net/c/278/m/{media_identity}.ism/",
+                    resolution,
+                    audio,
+                )
 
-                    representations = list(
-                        filter(
-                            lambda representation: representation["mime-type"]
-                            == "video/mp4"
-                            and representation["representation-id"] == vrepr_id
-                            or representation["mime-type"] == "audio/mp4"
-                            and representation["representation-id"] == arepr_id,
-                            representations,
-                        )
-                    )
-
-                    if len(representations) == 2:
-                        video_links: List[str] = []
-                        audio_links: List[str] = []
-
-                        for representation in representations:
-                            init_file = representation["init"]
-                            mime_type = representation["mime-type"]
-                            segments = representation["segments"]
-
-                            if mime_type == "video/mp4":
-                                video_links.append(init_file)
-                                video_links.extend(segments)
-                            elif mime_type == "audio/mp4":
-                                audio_links.append(init_file)
-                                audio_links.extend(segments)
-
-                        output_dir = "downloads"
-                        video_output_dir = f"{output_dir}/video"
-                        audio_output_dir = f"{output_dir}/audio"
-
-                        if not os.path.exists(output_dir):
-                            os.mkdir(output_dir)
-
-                        if not os.path.exists(video_output_dir):
-                            os.mkdir(video_output_dir)
-
-                        if not os.path.exists(audio_output_dir):
-                            os.mkdir(audio_output_dir)
-
-                        video_files = []
-                        for video_link in video_links:
-                            video_filename = video_link.split("/").pop()
-                            path = f"{video_output_dir}/{video_filename}"
-
-                            if not os.path.exists(path):
-                                download_file(video_link, path)
-                            else:
-                                console.print(f"File {path!r} already exists!")
-
-                            video_files.append(path)
-
-                        audio_files = []
-                        for audio_link in audio_links:
-                            audio_filename = audio_link.split("/").pop()
-                            path = f"{audio_output_dir}/{audio_filename}"
-
-                            if not os.path.exists(path):
-                                download_file(audio_link, path)
-                            else:
-                                console.print(f"File [cyan]{path!r}[/] already exists!")
-
-                            audio_files.append(path)
-
-                        if output is None:
-                            formatted_en_title = en_title.replace(" ", "-").lower()
-                            media_output_dir = f"{output_dir}/output"
-
-                            if not os.path.exists(media_output_dir):
-                                os.mkdir(media_output_dir)
-
-                            output = pathlib.Path(
-                                f"{media_output_dir}/{formatted_en_title}-{resolution}-{item_id}.mp4"
-                            )
-
-                        video_bytes: bytes = b""
-                        audio_bytes: bytes = b""
-
-                        for video_file in video_files:
-                            with open(video_file, "rb") as f:
-                                video_bytes += f.read()
-
-                        for audio_file in audio_files:
-                            with open(audio_file, "rb") as f:
-                                audio_bytes += f.read()
-
-                        with open(video_path := f"/tmp/{uuid.uuid4()}.mp4", "wb") as f:
-                            f.write(video_bytes)
-
-                        with open(audio_path := f"/tmp/{uuid.uuid4()}.mp3", "wb") as f:
-                            f.write(audio_bytes)
-
-                        if not os.path.exists(output):
-                            # Using ffmpeg-python to merge audio and video
-
-                            ffmpeg.output(
-                                ffmpeg.input(video_path),
-                                ffmpeg.input(audio_path),
-                                str(output),
-                                vcodec="copy",  # Copy the video stream without re-encoding
-                                acodec="aac",  # Encode the audio in AAC format
-                            ).run()
-                        else:
-                            logger.warning(f"File already exists! {str(output)!r}")
-
-                    else:
-                        logger.warning(
-                            "No matching video or audio representation ID was found. Please check the provided resolution or bitrate."
-                        )
+                if media:
+                    video_bytes, audio_bytes = media
+                    self.merge_media(video_bytes, audio_bytes, output)
