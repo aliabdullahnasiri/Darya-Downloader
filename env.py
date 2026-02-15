4bad52ff127bd11b0a45a2e0a6f42a50e092830f
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-02-15 10:23:56 +0430

    fixied.

--- env.py
+++ env.py
@@ -7,7 +7,11 @@ load_dotenv()
 
 class Meta(type):
     def __getattr__(cls, name):
-        return os.getenv(name, None)
+        assert (_ := os.getenv(name, None)), AttributeError(
+            f"type object {cls.__name__!r} has no attribute {name!r}"
+        )
+
+        return _
 
     def __getattribute__(cls, name):
         return super().__getattribute__(name)
